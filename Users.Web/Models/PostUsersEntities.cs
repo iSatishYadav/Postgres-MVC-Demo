92c321b660774905d9e6ff40f63ba753f98d9903
Satish Yadav <monstersatish@LIVE.COM>
2018-04-10 07:49:33 +0530

    Add project files.

---
+++ Users.Web/Models/PostUsersEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Users.Web.Models
+{
+    public class PostUsersEntities : DbContext
+    {
+        public PostUsersEntities() : base("UserPostEntities")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Users.Web/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Users.Web.Models
+{
+    [Table("user", Schema = "public")]
+    public class User
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("is_active")]
+        public bool IsActive { get; set; }
+    }
+}
